ed6e2088dd21d79ee7a0ed99568e0961eed2ef07
widmaker666 <114477330+widmaker666@users.noreply.github.com>
2022-10-18 13:16:42 +0200

    Add files via upload

--- app.rb
+++ app.rb
@@ -8,4 +8,27 @@ require_relative 'lib/player'
 player1 = Player.new("Merde")
 player2 = Player.new("Clanpin")
 
+while player1.life_points > 0 && player2.life_points >0 do
+    puts "
+   -----------
+   "
+  
+   puts "Voici l'état des combattants :"
+   player1.show_state
+   player2.show_state
+  
+   puts "
+   ---
+   "
+  
+   puts "Luttez ou mourrez :"
+  
+   player1.attacks(player2) # player 1 attaque
+   if player2.life_points <= 0 # pas de mort-vivants
+     break
+   end
+   player2.attacks(player1) # player 2 attaque
+  
+  end
+
 binding.pry
